f5f6e5c4fab83d4b5bcedbc6d99e35e3776188db
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-12-01 20:29:55 -0800

    that code doesn't actually work so fixed that

--- software/init.ino
+++ software/init.ino
@@ -9,7 +9,7 @@
 // It exploits features of GCC's preprocessor to generate a PROGMEM
 // table (in flash memory) holding an 8-bit unsigned sine wave (0-255).
 const int _SBASE_ = __COUNTER__ + 1; // Index of 1st __COUNTER__ ref below
-#define _S1_ (sin((__COUNTER__ - _SBASE_) / 128.0 * M_PI) + 1.0) * 127.5 + 0.5,
+#define _S1_ (uint8_t)((sin((__COUNTER__ - _SBASE_) / 128.0 * M_PI) + 1.0) * 127.5 + 0.5),
 #define _S2_ _S1_ _S1_ _S1_ _S1_ _S1_ _S1_ _S1_ _S1_       // Expands to 8 items
 #define _S3_ _S2_ _S2_ _S2_ _S2_ _S2_ _S2_ _S2_ _S2_       // Expands to 64 items
 const uint8_t PROGMEM sineTable[] = {_S3_ _S3_ _S3_ _S3_}; // 256 items
@@ -17,7 +17,7 @@ const uint8_t PROGMEM sineTable[] = {_S3_ _S3_ _S3_ _S3_}; // 256 items
 // Similar to above, but for an 8-bit gamma-correction table.
 #define _GAMMA_ 2.6
 const int _GBASE_ = __COUNTER__ + 1; // Index of 1st __COUNTER__ ref below
-#define _G1_ pow((__COUNTER__ - _GBASE_) / 255.0, _GAMMA_) * 255.0 + 0.5,
+#define _G1_ (uint8_t)(pow((__COUNTER__ - _GBASE_) / 255.0, _GAMMA_) * 255.0 + 0.5),
 #define _G2_ _G1_ _G1_ _G1_ _G1_ _G1_ _G1_ _G1_ _G1_        // Expands to 8 items
 #define _G3_ _G2_ _G2_ _G2_ _G2_ _G2_ _G2_ _G2_ _G2_        // Expands to 64 items
 const uint8_t PROGMEM gammaTable[] = {_G3_ _G3_ _G3_ _G3_}; // 256 items
