0d29e7a5732f2ceca5eef45618f431fc811e4134
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-12-18 22:49:34 -0500

    basic grid is in place, just need to be pulled in but too tired so tomorrow

--- software/static/main.js
+++ software/static/main.js
@@ -2,6 +2,16 @@ const root = document.querySelector('div#app-root');
 
 root.innerHTML = bootstrapApp({ title: `It's Lit` });
 
+function getBasicGrid(header, main, footer) {
+  return `
+    <div class="basic-grid">
+      <div class="grid-header">${header}</div>
+      <div class="grid-main">${main}</div>
+      <div class="grid-footer">${footer}</div>
+    </div>
+  `;
+}
+
 function getMainTemplate(props) {
   const { title } = props;
 
