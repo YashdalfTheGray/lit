433149a3e337e874e28a941807e66dd81770a12b
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-12-20 13:37:38 -0500

    added a bit more organization to the js file

--- software/static/main.js
+++ software/static/main.js
@@ -1,8 +1,22 @@
+// Find elements
 const root = document.querySelector('div#app-root');
-
 root.innerHTML = bootstrapApp({ title: `It's Lit` });
 
-function getBasicGrid(header, main, footer) {
+// boostrap
+function bootstrapApp(props) {
+  return buildMainTemplate(props);
+}
+
+// buidler functions
+function buildMainTemplate(props) {
+  const { title } = props;
+
+  const header = `<h2>${title}</h2>`;
+
+  return buildBasicGrid(header, '', '');
+}
+
+function buildBasicGrid(header, main, footer) {
   return `
     <div class="basic-grid">
       <div class="grid-header">${header}</div>
@@ -10,16 +24,4 @@ function getBasicGrid(header, main, footer) {
       <div class="grid-footer">${footer}</div>
     </div>
   `;
-}
-
-function getMainTemplate(props) {
-  const { title } = props;
-
-  const header = `<h2>${title}</h2>`;
-
-  return getBasicGrid(header, '', '');
-}
-
-function bootstrapApp(props) {
-  return getMainTemplate(props);
 }
