0cf911dc61937e27d791f46d24843d1daae0e119
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-12-20 13:56:04 -0500

    added some comments and some types, makes a bit more sense

--- software/static/main.js
+++ software/static/main.js
@@ -1,3 +1,10 @@
+// Type signatures
+/**
+ * @typedef MainProps
+ * @type {object}
+ * @property {string} title - the title of the app
+ */
+
 // Find elements
 const root = document.querySelector('div#app-root');
 root.innerHTML = bootstrapApp({ title: `It's Lit` });
@@ -8,12 +15,13 @@ function bootstrapApp(props) {
 }
 
 // buidler functions
+/**
+ * Builds the main template
+ * @param {MainProps} props the props that drive the app
+ * @returns {string} the template
+ */
 function buildMainTemplate(props) {
-  const { title } = props;
-
-  const header = `<h2>${title}</h2>`;
-
-  return buildBasicGrid(header, '', '');
+  return buildBasicGrid(buildHeader(props), '', '');
 }
 
 function buildBasicGrid(header, main, footer) {
@@ -22,6 +30,16 @@ function buildBasicGrid(header, main, footer) {
       <div class="grid-header">${header}</div>
       <div class="grid-main">${main}</div>
       <div class="grid-footer">${footer}</div>
+    </div>
+  `;
+}
+
+function buildHeader(props) {
+  const { title } = props;
+
+  return `
+    <div class="header">
+      <h2>${title}</h2>
     </div>
   `;
 }
