1afb048b59aa0c691772aa4369e450ad08dc3ff5
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2023-01-01 22:21:38 -0800

    added a dummy secrets file and the request library

---
+++ firmware/secrets.py
@@ -0,0 +1,7 @@
+secrets = {
+    'ssid': 'home_wifi_network',
+    'password': 'wifi_password',
+    'aio_username': 'my_adafruit_io_username',
+    'aio_key': 'my_adafruit_io_key',
+    'timezone': "America/New_York",  # http://worldtimeapi.org/timezones
+}
